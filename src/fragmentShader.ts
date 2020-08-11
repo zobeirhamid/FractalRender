35d4fd9d581322bb2966cd29ed57c094b5358925
Zobeir Hamid <zobeirhamid@berkeley.edu>
2020-08-10 21:54:29 -0700

    Adds uniform qualifiers and different colors

--- src/fragmentShader.ts
+++ src/fragmentShader.ts
@@ -1,41 +1,61 @@
 export default `
 precision mediump float;
+const int MAX_ITER = 2000;
+uniform int ITERATIONS;
+uniform vec2 offset;
+uniform float width;
+uniform float height;
+uniform vec4 boundaries;
 
 vec2 complexMultiplication(vec2 z1, vec2 z2)
 {
 	return vec2(z1.x * z2.x - z1.y * z2.y, z1.x * z2.y + z1.y * z2.x);
 }
 
-float mandelbrot(vec2 c)
+// https://github.com/hughsk/glsl-hsv2rgb/blob/master/index.glsl
+vec3 hsv2rgb(vec3 c) {
+  vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+  vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+  return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+int mandelbrot(vec2 c)
 {
 	vec2 z = vec2(0, 0);
-	int iterations = 0;
-	for (int i = 0; i < 2000; i++) {
+	int n = 0;
+	for (int i = 0; i < MAX_ITER; i++) {
 		if (length(z) > 2.0) {
 			break;
 		}
+		if (n > ITERATIONS) {
+			break;
+
+		}
 		z = complexMultiplication(z, z) + c;
-		iterations++;
+		n++;
 	}
-	return float(iterations);
+	return n;
 }
 
 void main(void)
 {
-	float x0 = -2.0;
-	float x1 = 1.0;
-	float y0 = -1.0;
-	float y1 = 1.0;
-
-	float width = 600.0;
-	float height = 400.0;
-
 	float x = gl_FragCoord.x;
 	float y = gl_FragCoord.y;
-	vec2 c = vec2(x0 + x * (x1 - x0) / (width), y0 + y * (y1 - y0) / (height));
+	vec2 c = vec2(offset.x + boundaries.x + x * (boundaries.y - boundaries.x) / (width), offset.y + boundaries.z + y * (boundaries.w - boundaries.z) / (height));
 
-	float m = mandelbrot(c);
+	int m = mandelbrot(c);
+	// VERSION 1
+	//int color = 1 - m / ITERATIONS;
 
-	gl_FragColor = vec4(255.0 / m, 255.0 / m, 255.0 / m, 1.0);
+	// VERSION 2
+	// float color = 1.0 - float(m) / float(ITERATIONS);
+	// gl_FragColor = vec4(color, color, color, 1.0);
+
+
+	// VERSION 3
+	float hue = float(m) / float(ITERATIONS);
+	float saturation = 1.0;
+	float value = m < ITERATIONS ? 1.0 : 0.0;
+	gl_FragColor = vec4(hsv2rgb(vec3(hue, saturation, value)), 1.0);
 }
 `;
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import initShaderProgram from "./initShaderProgram";
 
 const canvas = <HTMLCanvasElement>document.getElementById("webgl");
 
-canvas.width = 600;
-canvas.height = 400;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 const gl = <WebGLRenderingContext>canvas.getContext("webgl");
 
@@ -30,14 +30,66 @@ const verticesBufferObject = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, verticesBufferObject);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
-gl.vertexAttribPointer(
-  0,
-  3,
-  gl.FLOAT,
-  false,
-  3 * Float32Array.BYTES_PER_ELEMENT,
-  0
-);
-gl.enableVertexAttribArray(0);
+function paint(uniformLocations: any) {
+  gl.useProgram(shaderProgram);
+  gl.vertexAttribPointer(
+    0,
+    3,
+    gl.FLOAT,
+    false,
+    3 * Float32Array.BYTES_PER_ELEMENT,
+    0
+  );
+  gl.enableVertexAttribArray(0);
+  gl.uniform1i(
+    gl.getUniformLocation(shaderProgram, "ITERATIONS"),
+    uniformLocations.ITERATIONS
+  );
+  gl.uniform2fv(
+    gl.getUniformLocation(shaderProgram, "offset"),
+    uniformLocations.offset
+  );
+  gl.uniform1f(
+    gl.getUniformLocation(shaderProgram, "width"),
+    uniformLocations.width
+  );
+  gl.uniform1f(
+    gl.getUniformLocation(shaderProgram, "height"),
+    uniformLocations.height
+  );
+  gl.uniform4fv(
+    gl.getUniformLocation(shaderProgram, "boundaries"),
+    uniformLocations.boundaries
+  );
+  gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+}
 
-gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+window.addEventListener("resize", resize);
+
+function resize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  let width = canvas.width;
+  let height = (400 / 600) * canvas.width;
+
+  if (height > canvas.height) {
+    width = (600 / 400) * canvas.height;
+    height = canvas.height;
+  }
+
+  const uniformLocations = {
+    ITERATIONS: 50,
+    boundaries: [-2, 1, -1, 1],
+    offset: [
+      (-(canvas.width - width) / 2) * (3 / width),
+      (-(canvas.height - height) / 2) * (2 / height),
+    ],
+    width,
+    height,
+  };
+
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  paint(uniformLocations);
+}
+
+resize();
