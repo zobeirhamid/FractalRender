82c802def5ddebbe39898b367cd294fc1d3d54c8
Zobeir Hamid <zobeirhamid@berkeley.edu>
2020-08-11 20:23:38 -0700

    Updates

---
+++ precache-manifest.2ca4970a436631af6ccbe152c40ae823.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8743b75f9a810bd73cc4a4402203e55",
+    "url": "/FractalRender/index.html"
+  },
+  {
+    "revision": "d67c35cc7d9fe1c0e16e",
+    "url": "/FractalRender/static/css/2.460e1820.chunk.css"
+  },
+  {
+    "revision": "0dafa76acf2aa1515e00",
+    "url": "/FractalRender/static/css/main.e5873c1e.chunk.css"
+  },
+  {
+    "revision": "d67c35cc7d9fe1c0e16e",
+    "url": "/FractalRender/static/js/2.7a145728.chunk.js"
+  },
+  {
+    "revision": "9944a45902f915d9aae4db0c09e83634",
+    "url": "/FractalRender/static/js/2.7a145728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dafa76acf2aa1515e00",
+    "url": "/FractalRender/static/js/main.1d15ec86.chunk.js"
+  },
+  {
+    "revision": "82004fe93faebe94d452",
+    "url": "/FractalRender/static/js/runtime-main.dc945ae5.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/FractalRender/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/FractalRender/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/FractalRender/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/FractalRender/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/FractalRender/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/FractalRender/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
